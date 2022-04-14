4877837a9802a3022b39a7d523a0f2f40ca02792
sukso96100 <me@youngbin.xyz>
2022-04-14 18:08:42 +0900

    서버 시작 시 키클락 싱글톤 초기화

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"errors"
 	"fmt"
 	"iam/api"
+	"iam/clients"
 	"net/http"
+
+	"iam/middlewares"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
@@ -13,15 +16,13 @@ import (
 
 var ctx = context.Background()
 
-var clientID = "test_service"
-var clientSecret = "d7c2424e-7dfc-4a74-a6c5-bd6588ba2d73"
-var realm = "test_realm"
-var key_uri = "https://iam.cloudmt.co.kr"
-
 func main() {
 
+	clients.InitKeycloakClient(ctx)
+
 	route := gin.Default()
-	route.Use(introspect_middleware())
+
+	route.Use(middlewares.IntrospectMiddleware())
 
 	groups := route.Group("/groups")
 	{
@@ -43,40 +44,6 @@ func main() {
 	}
 
 	route.Run(":8085")
-}
-
-func introspect_middleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-
-		// 클라이언트가 보낸 토큰에 대한 keycloak 인증 부분입니다.
-		/*
-			token := c.Request.Header.Get("token")
-			if token == "" {
-				c.String(http.StatusUnauthorized, "Unauthorized")
-				panic("Unauthorized")
-			}
-
-			username := getUsernameJWT(token)
-			if username == "" {
-				c.String(http.StatusUnauthorized, "Unauthorized")
-				panic("Unauthorized")
-			}
-			// 여기서 구한 username 으로 권한 체크를 해야합니다.
-			// 다만 keycloak 설정에 따라 토큰의 내용이 변경될 수도 있으므로 이후 테스트 필요...
-
-			var client = gocloak.NewClient(key_uri)
-			rptResult, err := client.RetrospectToken(token, clientID, clientSecret, realm)
-			if err != nil {
-				c.String(http.StatusUnauthorized, "Unauthorized")
-				panic("Unauthorized")
-			}
-
-			if !rptResult.Active {
-				c.String(http.StatusUnauthorized, "Unauthorized")
-				panic("Unauthorized")
-			}
-		*/
-	}
 }
 
 func badRequeset(w http.ResponseWriter, r *http.Request) {
