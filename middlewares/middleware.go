ee1a5228fd4a9d632a81ed63491c9054949ae4bf
sukso96100 <me@youngbin.xyz>
2022-04-14 18:08:08 +0900

    토큰 검증 미들웨어를 별도 패키지로 분리 #542

---
+++ middlewares/middleware.go
@@ -0,0 +1,60 @@
+package middlewares
+
+import (
+	"fmt"
+	"iam/clients"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func IntrospectMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		auth := c.Request.Header.Get("Authorization")
+		if auth == "" {
+			c.String(http.StatusForbidden, "No Authorization header provided")
+			c.Abort()
+			return
+		}
+		token := strings.TrimPrefix(auth, "Bearer ")
+		result, err := clients.KeycloakClient().RetrospectToken(c, token, clients.KeycloakClientID, clients.KeycloakClientSecret, clients.KeycloakRealm)
+		fmt.Print(result)
+		if err != nil {
+			c.String(http.StatusForbidden, "Authorization is not valid")
+			c.Abort()
+			panic("Inspection failed:" + err.Error())
+		}
+
+		c.Set("accessToken", token)
+
+		// 클라이언트가 보낸 토큰에 대한 keycloak 인증 부분입니다.
+		/*
+			token := c.Request.Header.Get("token")
+			if token == "" {
+				c.String(http.StatusUnauthorized, "Unauthorized")
+				panic("Unauthorized")
+			}
+
+			username := getUsernameJWT(token)
+			if username == "" {
+				c.String(http.StatusUnauthorized, "Unauthorized")
+				panic("Unauthorized")
+			}
+			// 여기서 구한 username 으로 권한 체크를 해야합니다.
+			// 다만 keycloak 설정에 따라 토큰의 내용이 변경될 수도 있으므로 이후 테스트 필요...
+
+			var client = gocloak.NewClient(key_uri)
+			rptResult, err := client.RetrospectToken(token, clientID, clientSecret, realm)
+			if err != nil {
+				c.String(http.StatusUnauthorized, "Unauthorized")
+				panic("Unauthorized")
+			}
+
+			if !rptResult.Active {
+				c.String(http.StatusUnauthorized, "Unauthorized")
+				panic("Unauthorized")
+			}
+		*/
+	}
+}
