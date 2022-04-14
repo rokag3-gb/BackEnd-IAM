4b9392bac438079c6546070aa2d47e5984514895
sukso96100 <me@youngbin.xyz>
2022-04-14 18:09:12 +0900

    WIP: Group 목록 조회 API #542

--- api/groups.go
+++ api/groups.go
@@ -1,13 +1,23 @@
 package api
 
 import (
+	"iam/clients"
 	"net/http"
 
+	"github.com/Nerzal/gocloak/v11"
 	"github.com/gin-gonic/gin"
 )
 
 func GetGroup(c *gin.Context) {
-	c.String(http.StatusOK, "getgroup")
+	token := c.MustGet("accessToken").(string)
+
+	groups, err := clients.KeycloakClient().GetGroups(c, token, clients.KeycloakRealm, gocloak.GetGroupsParams{})
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, groups)
+	// c.String(http.StatusOK, "getgroup")
 }
 
 func CreateGroup(c *gin.Context) {
