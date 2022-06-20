4b5c6e5b24ba0ae649556106a89fa0b7928c0331
Youngbin Han <youngbin.han@cloudmt.co.kr>
2022-06-20 09:50:41 +0900

    code API 라우팅 설정

---
+++ api/code.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GetCodeItem(c *gin.Context) {
+
+}
+
+func GetCodeChilds(c *gin.Context) {
+
+}
+
+func CreateCodeItem(c *gin.Context) {
+
+}
+
+func UpdateCodeItem(c *gin.Context) {
+}
+
+func DeleteCodeItem(c *gin.Context) {
+}
